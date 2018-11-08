6d5d40c1d973b1f9cf0fad8ceb3948a00de2cf58
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-11-07 21:54:28 -0600

    Setup AntiAdvertising

--- src/EmeraldMC/Core.php
+++ src/EmeraldMC/Core.php
@@ -1,8 +1,14 @@
 <php?
 
-*/ 
-EmeraldMC | SythicDevs
-Coming Soon Under Maintanence
-*/
+namespace EmeraldMC;
 
-namespace EmeraldMC\Core;
+use EmeraldMC\anti\AntiAdvertising;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+
+class Core extends PluginBase{
+      
+      public function onEnable() : void{
+      $this->getServer()->getPluginManager()-registerEvents(($this->antiadvertising = new AntiAdvertising()), $this);
+  }
+}
